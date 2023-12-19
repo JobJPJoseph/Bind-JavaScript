a490c7e768821cf381f9a4fea7fee0c9708df897
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2023-12-18 19:46:22 -0500

    We learned about binding a context in JS.

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 // Your code here
+
+const { Employee } = require('./employee');
+
+const employee = new Employee('John Wick', 'Dog Lover');
+
+// const employeeName  = employee.sayName;
+const employeeName = employee.sayName.bind(employee);
+
+global.setTimeout(employeeName, 2000);
+
+// const employeeOccupation = employee.sayOccupation;
+const employeeOccupation = employee.sayOccupation.bind(employee);
+
+global.setTimeout(employeeOccupation, 3000);
